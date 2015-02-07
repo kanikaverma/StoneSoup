16610d117eee228b75b52f771fa6204107ac777d
Kanika Verma <vermakanika@hotmail.com>
2015-02-07 03:25:32 -0500

    displaying correctly with pictures and nonfunctional shopping list button

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-import requests, unirest
+import requests
 
 app = Flask(__name__)
 app.config["DEBUG"]=True
@@ -16,12 +16,27 @@ def hellothere():
 #<this is a variable> it can be overloaded into the function! wow!
 @app.route('/home/<food_query>')
 def getNews(food_query):
-	response = requests.get("https://community-food2fork.p.mashape.com/search?key=fb087049410336a1a564b4d90772884a&q=%s",
+	response = requests.get("https://community-food2fork.p.mashape.com/search?key=fb087049410336a1a564b4d90772884a&q=%s"% food_query,
 	                       headers = {
 	"X-Mashape-Key": "wBtGgGCJ65mshgqXuQksMa9vpohbp1RzC3AjsnKXEHKeWKqZH3",
 	"Accept": "application/json"
   }).json()
-  	return jsonify(response)
+
+	recipeList=""
+	for x in response["recipes"]:
+		recipeList+="<a href=\""+x["source_url"]+"\">"+x["title"]+"</a><br><img src=\""+x["image_url"]+"\"/>"+"<br><button id=\""+x["recipe_id"]+"\">Shopping List</button>""<br><br>"
+	
+	recipe_id=response["recipes"][0]["recipe_id"]
+	recipe_url=response["recipes"][0]["source_url"]
+	recipe_title=response["recipes"][0]["title"]
+	print recipe_title
+	print recipe_id
+	print recipe_url
+
+  	return recipeList
+
+
+
 
 @app.errorhandler(404)
 def page_not_found(error):
