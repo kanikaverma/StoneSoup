ed4578f436ca523d2e7fc9dd299741f824e0785f
Kanika Verma <vermakanika@hotmail.com>
2015-02-07 05:51:16 -0500

    nicely formatted ready to go

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import requests
 
 app = Flask(__name__)
@@ -10,7 +10,7 @@ def hello():
 
 @app.route('/home')
 def hellothere():
-	return "<h1>Homepage boi<h1><h2>of Stone Soup</h2><p>Welcome to our Kitchen!</p>"
+	return "<h1>StoneSoup<h1><h2>Welcome to our kitchen!</h2><p>Here's what you should make today!</p>"
 
 #dynamic routes with dynamicness! <define variable>
 #<this is a variable> it can be overloaded into the function! wow!
@@ -22,18 +22,14 @@ def getNews(food_query):
 	"Accept": "application/json"
   }).json()
 
-	recipeList=""
+	recipeList="<html><head><link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css\"><title size=\"20\">StoneSoup</title></head><body bgcolor=\"7fffd4\"><center><h1>StoneSoup<h1><h2>Welcome to our kitchen!</h2><p>Here's what you should make today!</p>"
 	for x in response["recipes"]:
-		recipeList+="<a href=\""+x["source_url"]+"\">"+x["title"]+"</a><br><img src=\""+x["image_url"]+"\"/>"+"<br><button id=\""+x["recipe_id"]+"\">Shopping List</button>""<br><br>"
+		recipeList+="<h3><a href=\""+x["source_url"]+"\">"+x["title"]+"</a></h3><br><img src=\""+x["image_url"]+"\"/>"+"<br><br>"
 	
-	recipe_id=response["recipes"][0]["recipe_id"]
-	recipe_url=response["recipes"][0]["source_url"]
-	recipe_title=response["recipes"][0]["title"]
-	print recipe_title
-	print recipe_id
-	print recipe_url
 
-  	return recipeList
+	recipeList+="</center></body></html>"
+	return recipeList
+  	#return render_template("StoneSoup.html", recipeList = recipeList)
 
 
 
