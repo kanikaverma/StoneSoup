71782188e625796fdbc6fbb5a4c985d3d3407ea0
Eunice Emefa Kokor <eek2138@columbia.edu>
2015-12-31 21:52:19 -0800

    all on one page

--- app.py
+++ app.py
@@ -6,28 +6,24 @@ app.config["DEBUG"]=True
 
 @app.route('/')
 def hello():
-	return render_template('index.html')
+	return render_template('search.html', recipes="initial")
 
 @app.route('/search')
 def search():
-  return render_template('search.html')
+  return render_template('search.html', recipes="initial")
 
 @app.route('/results', methods=["GET", "POST"])
 def get_results():
   food_query = request.form['text']
+  food_query = food_query.replace(" ", "+")
   food_query_processed = food_query.lower()
-  return redirect('/home/'+ food_query_processed)
-
-#dynamic routes with dynamicness! <define variable>
-#<this is a variable> it can be overloaded into the function! wow!
-@app.route('/home/<food_query>')
-def getNews(food_query):
-  response = requests.get("https://community-food2fork.p.mashape.com/search?key=fb087049410336a1a564b4d90772884a&q=%s"% food_query, headers = {
+  request_string = "https://community-food2fork.p.mashape.com/search?key=fb087049410336a1a564b4d90772884a&q={}".format(food_query_processed)
+  response = requests.get(request_string, headers = {
   "X-Mashape-Key": "wBtGgGCJ65mshgqXuQksMa9vpohbp1RzC3AjsnKXEHKeWKqZH3",
   "Accept": "application/json"
   }).json()
   recipes = response['recipes']
-  return render_template("results.html", recipes=recipes)
+  return render_template('search.html', recipes=recipes)
 
 @app.errorhandler(404)
 def page_not_found(error):
